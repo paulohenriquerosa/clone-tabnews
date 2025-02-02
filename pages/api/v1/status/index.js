0be1f08e1a23be31ca466c8c9ec9c709c208b812
Paulo Henrique Rosa <paulohenriquerosa04@gmail.com>
2025-02-02 17:15:29 -0300

    add database and .env files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function stauts(request, response){
+import database from "../../../../infra/database.js"
+
+async function stauts(request, response){
+    const result = await database.query("SELECT 1+1 AS SUM;")
+    console.log(result.rows)
     return response.status(200).json({chave: "valor"})
 }
 
