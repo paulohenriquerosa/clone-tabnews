01b871ff8c56dc31a4b31cfc02c5a84c8bac11b5
Paulo Henrique Rosa <paulohenriquerosa04@gmail.com>
2025-04-13 06:36:40 -0300

    style: prittier issues

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -5,15 +5,16 @@ beforeAll(async () => {
   await orchestrator.clearDatabase();
 });
 
-
 describe("POST /api/v1/migrations", () => {
   describe("Anonymous User", () => {
     describe("Running pending migrations", () => {
-
       test("For the first time", async () => {
-        const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-          method: "POST",
-        });
+        const response1 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
 
         expect(response1.status).toBe(201);
 
@@ -21,13 +22,15 @@ describe("POST /api/v1/migrations", () => {
 
         expect(Array.isArray(responseBody1)).toBe(true);
         expect(responseBody1.length).toBeGreaterThan(0);
-
       });
 
       test("For the second time", async () => {
-        const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-          method: "POST",
-        });
+        const response2 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
 
         expect(response2.status).toBe(200);
 
@@ -36,7 +39,6 @@ describe("POST /api/v1/migrations", () => {
         expect(Array.isArray(responseBody2)).toBe(true);
         expect(responseBody2.length).toBe(0);
       });
-    })
-  })
-})
-
+    });
+  });
+});
